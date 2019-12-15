609aa3d1768178c6b877eeca0cb0c530035933dc
FStolarczyk99 <58258355+FStolarczyk99@users.noreply.github.com>
2019-12-15 13:14:38 +0100

    Klasa Package + alias ElementID

--- package.h
+++ package.h
@@ -1,9 +1,20 @@
 #ifndef NETSIM_PACKAGE_H
 #define NETSIM_PACKAGE_H
 
+#include "types.h"
+#include <set>
 
-class package {
 
+class Package {
+public:
+    Package();
+    Package(Package&&) = default;
+    int get_id() const { return id_; }
+    ~Package();
+private:
+    ElementID id_;
+    static std::set<ElementID> assigned_IDs;
+    static std::set<ElementID> freed_IDs;
 };
 
 
