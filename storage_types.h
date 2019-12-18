db2138f95b41dda929802ea0b387bf8f24e10e28
ajlasuliga <49711328+ajlasuliga@users.noreply.github.com>
2019-12-18 13:40:55 +0100

    Update storage_types.h

--- storage_types.h
+++ storage_types.h
@@ -7,15 +7,18 @@ class IPackageStockpile {
 };
 
 class IPackageQueue {
-
+public:
+    virtual Package pop() = 0;
+    virtual PackageQueueType get_queue_type() const = 0;
 };
 
 class PackageQueue {
 
 };
 
-class PackageQueueType {
-
+enum class PackageQueueType {
+    FIFO,
+    LIFO
 };
 
 #endif //NETSIM_STORAGE_TYPES_H
