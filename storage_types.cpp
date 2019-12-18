9b10fdf49269f941b296b5323bde5753282a1464
ajlasuliga <49711328+ajlasuliga@users.noreply.github.com>
2019-12-18 19:09:58 +0100

    Update storage_types.cpp

--- storage_types.cpp
+++ storage_types.cpp
@@ -1,1 +1,20 @@
 #include "storage_types.h"
+
+Package PackageQueue::pop() {
+
+    switch(queueType){
+        case PackageQueueType::FIFO:{
+            Package returnedPackage = std::move(queue.front());
+            queue.pop_front();
+            return returnedPackage;
+        }
+
+        case PackageQueueType ::LIFO: {
+            Package returnedPackage = std::move(queue.back());
+            queue.pop_back();
+            return returnedPackage;
+        }
+    }
+    return Package();
+}
+
