80f6983160a9d6c90f42c673dddc1abf05475883
FStolarczyk99 <58258355+FStolarczyk99@users.noreply.github.com>
2019-12-28 15:37:02 +0100

    ??

--- package.cpp
+++ package.cpp
@@ -1,28 +1,73 @@
 #include "package.h"
 
-Package::Package() {
-    if (freed_IDs.empty()) {
-        assigned_IDs.insert(assigned_IDs.size() + 1);
-        id_ = assigned_IDs.size();
-    }
-    else {
-        auto tmp = freed_IDs.begin();
-        id_ = *tmp;
-        freed_IDs.erase(*tmp);
-        assigned_IDs.insert(*tmp);
+#include <stdexcept>
+
+void insert_if_not_exists(std::set<ElementID>& s, ElementID e) {
+    if (s.find(e) == s.end()) {
+        s.insert(e);
     }
 }
 
-//FIXME
-/*
-Package& Package::operator=(class Package pack) {
-    pack.id_ = 0;
+void erase_if_exists(std::set<ElementID>& s, ElementID e) {
+    if (s.find(e) != s.end()) {
+        s.erase(e);
+    }
 }
-*/
-std::set<ElementID> Package::assigned_IDs;
-std::set<ElementID> Package::freed_IDs;
+
+Package::Package() {
+    if (freed_ids_.empty()) {
+        // Przypisz najmniejszy dotychczas nieużyty identyfikator.
+        id_ = (*assigned_ids_.rbegin()) + 1;
+    } else {
+        // Metoda set::extract() nie jest dostępna we wszystkich implementacjach kompilatora C++.
+//            id_ = freed_ids_.extract(freed_ids_.begin());
+        id_ = *freed_ids_.begin();
+        freed_ids_.erase(id_);
+    }
+    assigned_ids_.insert(id_);
+}
+
+Package::Package(ElementID id) {
+    if (assigned_ids_.find(id) != assigned_ids_.end()) {
+        throw std::invalid_argument("The ID of " + std::to_string(id) + " is already assigned!");
+    }
+
+    id_ = id;
+    assigned_ids_.insert(id_);
+    erase_if_exists(freed_ids_, id);
+}
+
+Package::Package(Package&& other) noexcept {
+    id_ = other.id_;
+    other.id_ = BLANK_ID;
+}
+
+Package& Package::operator=(Package&& other) noexcept {
+    /*
+     * Przypisanie odbywa się do już istniejącego obiektu, zatem należy zadbać,
+     * aby przydzielone temu obiektowi ID wróciło do puli dostępnych ID.
+     * W przeciwnym razie m.in. każda operacja typu
+     *   Product p0;
+     *   Product p = p0;
+     * prowadziłaby do "wycieku identyfikatorów" - gdyż po jej wykonaniu ID p0
+     * wynosiłoby BLANK_ID, p otrzymałoby dotychczasowy ID p0, natomiast
+     * dotychczasowy ID p pozostałby w puli przydzielonych ID.
+     */
+    if (id_ != BLANK_ID) {
+        freed_ids_.insert(id_);
+        assigned_ids_.erase(id_);
+    }
+
+    id_ = other.id_;
+    other.id_ = BLANK_ID;
+
+    return *this;
+}
+
 
 Package::~Package() {
-    assigned_IDs.erase(id_);
-    freed_IDs.insert(id_);
+    if (id_ != BLANK_ID) {
+        insert_if_not_exists(freed_ids_, id_);
+        erase_if_exists(assigned_ids_, id_);
+    }
 }
--- package.h
+++ package.h
@@ -1,22 +1,54 @@
-#ifndef NETSIM_PACKAGE_H
-#define NETSIM_PACKAGE_H
+#ifndef NET_SIMULATION_PACKAGE_HPP
+#define NET_SIMULATION_PACKAGE_HPP
 
-#include "types.h"
 #include <set>
 
+#include "types.h"
 
+/**
+ * Ponieważ w myśl założeń projektowych w systemie nie mogą istnieć dwa
+ * półprodukty o tym samym ID, konstruktor kopiujący oraz operator przypisania
+ * zostały oznaczone jako usunięte.
+ *
+ * Ponieważ konstruktor przenoszący oraz przenoszący operator przypisania
+ * sprawiają, że obiekt przekazany jako odpowiednio argument lub operand
+ * przestaje być "poprawny" (nie powinniśmy się do niego odnosić później
+ * w programie), możemy ten obiekt zmodyfikować - oznaczyć jako "niepoprawny"
+ * poprzez przypisanie mu pewnego szczególnego ID spoza dopuszczalnego zakresu
+ * (dla przypomnienia: dopuszczalne ID to liczby całkowite DODATNIE).
+ */
 class Package {
 public:
     Package();
-    Package(Package&&) = default;
-    Package operator=(const Package& pack);
-    int get_id() const { return id_; }
+
+    explicit Package(ElementID id);
+
+    Package(const Package&) = delete;
+    Package(Package&&) noexcept;
+
+    Package& operator=(Package&) = delete;
+    Package& operator=(Package&&) noexcept;
+
     ~Package();
+
+    ElementID get_id() const { return id_; }
+
 private:
-    ElementID id_;
-    static std::set<ElementID> assigned_IDs;
-    static std::set<ElementID> freed_IDs;
+    static const ElementID BLANK_ID = -1;
+
+    /*
+     * Począwszy od C++17 pola statyczne mogą być oznaczane jako `inline`,
+     * dzięki czemu moża je inicjalizować wewnątrz definicji klasy (a nie
+     * w osobnym pliku źródłowym).
+     */
+
+    // Pula identyfikatorów aktualnie przyporządkowanych półproduktom.
+    inline static std::set<ElementID> assigned_ids_{0};
+
+    // Pula identyfikatorów, które zostały zwolnione w wyniku usuwania półproduktów.
+    inline static std::set<ElementID> freed_ids_;
+
+    ElementID id_ = BLANK_ID;
 };
 
-
-#endif //NETSIM_PACKAGE_H
+#endif //NET_SIMULATION_PACKAGE_HPP
