3528411ffe1bd9cceec18a48ddec92edb3c145e8
HSujka1 <58258501+HSujka1@users.noreply.github.com>
2020-01-04 16:16:41 +0100

    +worker

--- nodes.cpp
+++ nodes.cpp
@@ -41,3 +41,39 @@ void Storehouse::receivePackage(Package package)
 {
     depot->push(package);
 }
+
+Worker::Worker(ElementID _id, TimeOffset _time, std::unique_ptr<IPackageQueue> ptr)
+        :id(_id), processingDuration(_time)
+{
+    queue = std::move(ptr);
+}
+
+void Worker::doWork()
+{
+    if (currentlyProcessedPackage.size() == 0)
+    {
+        currentlyProcessedPackage.push_back(queue->pop());
+        packageProcessingStartTime = 1;
+    }
+    else
+    {
+        if (packageProcessingStartTime<processingDuration)
+        {
+            packageProcessingStartTime++;
+        }
+        else
+        {
+            sendingBuffer.push_back(queue->pop());
+            if (queue->size()==0)
+            {
+                packageProcessingStartTime = 0;
+            }
+            else
+            {
+                packageProcessingStartTime = 1;
+                currentlyProcessedPackage.push_back(queue->pop());
+            }
+
+        }
+    }
+}
